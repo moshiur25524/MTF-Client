51f1b91cabbafcadf5d1b9a22aaeec5873a4f68f
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-12-13 00:41:56 +0600

    custom hook named useAdmin to get the admin in boolean

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
+    const [user, loading, error] = useAuthState(auth);
+    const [admin] = useAdmin(user)
     return (
         <div className="drawer drawer-mobile">
             <input id="dasahboard-drawer" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
                 <h1 className='text-center text-3xl font-bold text-accent'>Welcome to Dashboard</h1>
                 <Outlet />
-                
+
             </div>
             <div className="drawer-side">
                 <label htmlFor="dasahboard-drawer" className="drawer-overlay"></label>
@@ -17,7 +22,7 @@ const Dashboard = () => {
                     <li><Link to='/dashboard'>My Appointments</Link></li>
                     <li><Link to='/dashboard/reviews'>My Reviews</Link></li>
                     <li><Link to='/dashboard/myHistory'>My History</Link></li>
-                    <li><Link to='/dashboard/users'>All Users</Link></li>
+                    {admin && <li><Link to='/dashboard/users'>All Users</Link></li>}
                 </ul>
 
             </div>
